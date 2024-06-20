b92ed7e04b89f2b2165dda156e1c3c3b39cad4f1
muhme <github@heikol.de>
2024-06-20 07:44:21 +0200

    Also delete the Docker network with clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
-# scripts/clean.sh - delete all quote_wp_* docker containers
+# scripts/clean.sh - delete all Docker containers and the network
+#                    or use 'docker compose down'
 #
 # GPLv3 License, Copyright (c) 2023 - 2024 Heiko Lübbe
 # WordPress-plugin random-quote-zitat-service, see https://github.com/muhme/quote_wordpress
 
-echo '*** Removing all docker containers quote_wp_*'
-docker ps -a --format '{{.Names}}' | grep '^quote_wp_' | xargs -r docker rm -f
+PREFIX="quote_wp_"
+NETWORK_NAME="quote_wordpress_default"
+
+echo '*** Remove following Docker containers'
+docker ps -a --format '{{.Names}}' | grep "^${PREFIX}" | xargs -r docker rm -f
+
+echo '*** Remove following Docker network'
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  docker network rm "$NETWORK_NAME"
+fi
